9c209be2c63120b1aa9f5efd9aa9e2163a696939
sudong97 <tkddlsqkr21@gmail.com>
2023-07-08 23:09:30 +0900

    [commit] first commit Stream study

---
+++ src/main/java/com/example/streamstudy/StreamStudy.java
@@ -0,0 +1,36 @@
+package com.example.streamstudy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+public class StreamStudy {
+    public static void main(String[] args){
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH)
+        );
+
+//        List<String> threeHighCaloricDishNames =
+//                menu.stream()
+//                        .filter(dish -> dish.getCalories() > 300)
+//                        .map(Dish::getName)
+//                        .limit(3)
+//                        .collect(toList());
+        List<String> menuName = menu.stream()
+                .filter(Dish -> Dish.getCalories() > 300)
+                .map(Dish::getName)
+                .collect(toList());
+        System.out.println("menuName = " + menuName);
+
+    }
+}
